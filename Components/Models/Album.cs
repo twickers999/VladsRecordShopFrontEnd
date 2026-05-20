d9c390b57c25d979400e693e82a913090a471648
twickers999 <vlad.inbox1999@gmail.com>
2026-05-20 14:44:01 +0100

    Set up connection to API using a frontend and create editable record details

---
+++ Components/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int[] ArtistId { get; set; }
+        public int Price { get; set; }
+        //public Song[] Songs { get; set; }
+    }
+
+    public class AlbumDto
+    {
+        public int? Year { get; set; }
+        public string? Name { get; set; }
+        public string? Genre { get; set; }
+        public int[]? ArtistId { get; set; }
+        public int? Price { get; set; }
+    }
+}
---
+++ Components/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
